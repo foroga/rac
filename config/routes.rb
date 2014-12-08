ec34ddc3cf8946936c5bc3d6d58f1c8c7b162b59
foroga <oscar.reyg@gmail.com>
2014-12-09 00:10:56 +0100

    i18n es|en

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
-  resources :users
+#xok,1c3  resources :users
+	scope "(:locale)", locale: /en|es/ do
+		resources :users
+	end
 end
